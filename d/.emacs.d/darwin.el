39ad6fe5313309cf052b77c2127ae0c5b32acbcc
Drew Raines <aaraines@gmail.com>
2010-04-29 11:29:07 -0500

    Don't swap CMD/OPT.  Let OS X do it for us.

--- d/.emacs.d/darwin.el
+++ d/.emacs.d/darwin.el
@@ -5,11 +5,17 @@
 (add-to-list 'exec-path "/usr/local/bin")
 
 (when (>= emacs-major-version 23)
-  ;(setq mac-command-modifier 'meta)
-  ;(setq mac-option-modifier 'super)
-  (custom-set-variables
-   '(ns-alternate-modifier (quote super))
-   '(ns-command-modifier (quote meta)))
+  ;; (setq mac-command-modifier 'meta)
+  ;; (setq mac-option-modifier 'super)
+
+  ;; or:
+
+  ;; (custom-set-variables
+  ;;  '(ns-alternate-modifier (quote super))
+  ;;  '(ns-command-modifier (quote meta)))
+
+  ;; Or do neither of these and switch CMD/OPT in OS X keyboard prefs
+
   (set-face-font 'default
 		 "-apple-Menlo-medium-normal-normal-*-12-*-*-*-m-0-iso10646-1")
   (global-set-key "\M-`" 'other-frame))
