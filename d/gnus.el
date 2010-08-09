69628d1da2d7498accfa2e2aaa405aed41f9aec8
Drew Raines <aaraines@gmail.com>
2010-08-09 09:20:58 -0500

    Add custom gnus opts.

--- d/gnus.el
+++ d/gnus.el
@@ -1,3 +1,52 @@
+(setq  user-full-name "Drew Raines"
+       user-mail-address "aaraines@gmail.com"
+       message-user-fqdn "id.draines.com"
+       gnus-check-bogus-newsgroups nil
+       gnus-check-new-newsgroups nil
+       gnus-article-skip-boring nil
+       gnus-delay-default-delay "2h"
+       gnus-gcc-mark-as-read nil
+       gnus-gcc-externalize-attachments "all"
+       gnus-home-score-file 'gnus-hierarchial-home-score-file
+       gnus-large-newsgroup 250
+       gnus-score-find-score-files-function 'gnus-score-find-hierarchical
+       gnus-treat-display-smileys t
+       gnus-treat-wash-html nil
+       gnus-treat-fill-long-lines nil   ;90
+       gnus-version-expose-system t
+       gnus-cited-lines-visible '(3 . 6)
+       gnus-article-skip-boring nil
+       gnus-treat-hide-citation t
+       message-cite-function 'message-cite-original-without-signature
+       message-dont-reply-to-names
+       (concat "\\(aaraines\\|me\\)@\\(gmail\\|draines\\)")
+       message-from-style 'angles
+       message-forward-as-mime nil
+       message-make-forward-subject-function 'message-forward-subject-fwd
+       message-subject-trailing-was-query nil
+       message-wash-forwarded-subjects t
+       gnus-auto-select-next 'almost-quietly
+       gnus-use-full-window nil ;; trying this while I keep org in the
+                                ;; other half of the screen
+       gnus-inhibit-mime-unbuttonizing t
+       nntp-marks-is-evil nil
+       nnml-marks-is-evil nil
+       gnus-use-cache t
+       gnus-user-agent '(emacs gnus config)
+       message-sendmail-envelope-from 'header
+       gnus-visible-headers
+       '("^From:" "^Newsgroups:" "^Subject:"
+         "^Date:" "^Followup-To:" "^Reply-To:"
+         "^Organization:" "^Summary:" "^Keywords:"
+         "^To:" "^Cc:" "^Posted-To:" "^Mail-Copies-To:"
+         "^Apparently-To:" "^Gnus-Warning:" "^Resent-From:"
+         "^X-Sent:" "^User-Agent:" "^X-Mailer:"
+         "^X-Newsreader:" "^X-Accept-Language:"
+         "^X-Delivery-Agent:"
+         "^X-Spambayes-Classification:" "^X-Spambayes-Trained:"
+         "^X-Bogosity:" "^X-Gnus-Delayed:"
+         "^Content-Type:"))
+
 (setq gnus-select-method '(nnnil ""))
 
 (setq gnus-secondary-select-methods
