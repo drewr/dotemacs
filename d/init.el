16dfcf24d5453a4b81b232d6f267a02db4eb23b4
Drew Raines <aaraines@gmail.com>
2010-11-25 14:26:24 -0600

    Shrink erc cols to 86.

--- d/init.el
+++ d/init.el
@@ -197,7 +197,7 @@
        erc-nick "drewr"
        erc-user-full-name "Drew Raines"
        erc-email-userid "drew"       ; for when ident is not activated
-       erc-fill-column 93
+       erc-fill-column 86
        erc-fill-prefix "   "
        erc-auto-query 'window-noselect
        erc-prompt-for-nickserv-password nil
