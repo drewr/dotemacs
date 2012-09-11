bb2036f0fba2d4911b136e37deebbc7b1b4bb17a
Drew Raines <aaraines@gmail.com>
2012-09-11 10:25:51 -0500

    Shrink gui font.

--- d/linux.el
+++ d/linux.el
@@ -1,6 +1,6 @@
 (setenv "PATH" (concat (expand-file-name "~/bin") ":"
                        (getenv "PATH")))
 
-(set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 105)
+(set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 102)
 
 (setq browse-url-browser-function 'browse-url-chromium)
