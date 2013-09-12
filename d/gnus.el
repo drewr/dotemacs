c992f0921a224bc7d399443b3a05a4513644b258
Andrew Raines <andrew@raines.me>
2013-09-12 14:33:16 -0500

    Don't use local mail sync.

--- d/gnus.el
+++ d/gnus.el
@@ -64,23 +64,14 @@
 
 (setq gnus-secondary-select-methods
       '((nnimap "aaraines"
-                (nnimap-address "imap.aaraines")
+                (nnimap-address "imap.gmail.com")
                 (nnimap-authenticator login))
         (nnimap "drewres"
-                (nnimap-address "imap.drewres")
+                (nnimap-address "imap.gmail.com")
                 (nnimap-authenticator login))
         (nnimap "testdroid1000"
-                (nnimap-address "imap.testdroid1000")
+                (nnimap-address "imap.gmail.com")
                 (nnimap-authenticator login))
-        (nnml ""
-              (nnir-search-engine swish-e)
-              (nnir-swish-e-remove-prefix "/home/aar/Mail/")
-              (nnir-swish-e-index-file "/home/aar/Mail/index.swish-e"))
-        (nnfolder "mboxes"
-                  (nnfolder-directory   "~/Mail/mboxes")
-                  (nnfolder-active-file "~/Mail/mboxes/active")
-                  (nnfolder-get-new-mail nil)
-                  (nnfolder-inhibit-expiry nil))
         (nntp "news.gmane.org")
         (nntp "news.gwene.org")))
 
