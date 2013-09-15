3780e1e40f2c32e0625ab8b2db6327c1164e55b6
Andrew Raines <aar@epsilon.local>
2013-09-14 21:05:38 -0500

    Add package repos.

--- d/init.el
+++ d/init.el
@@ -29,6 +29,10 @@
 (require 'ffap)
 (require 'saveplace)
 (require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 (toggle-uniquify-buffer-names)
 (recentf-mode 1)
