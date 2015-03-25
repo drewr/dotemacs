a97825ecfe0024ecba3e711c46f789fd8f5c1b05
Drew Raines <drew@raines.me>
2015-03-24 23:05:09 -0500

    Make delete-trailing-whitespace local to haskell-mode

--- d/init.el
+++ d/init.el
@@ -182,7 +182,8 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook '(lambda ()
                                 (add-hook 'before-save-hook
-                                          'delete-trailing-whitespace)))
+                                          'delete-trailing-whitespace
+                                          nil t)))
 
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
