6e875a1126bc053ac6ce5b690a50e20aebc85c52
Drew Raines <drew@raines.me>
2015-05-14 13:54:40 -0500

    Update packages

--- d/setup.el
+++ d/setup.el
@@ -2,13 +2,12 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+             '("melpa" . "http://stable.milkbox.net/packages/"))
 (package-initialize)
 (package-refresh-contents)
 (package-install 's)
 (package-install 'dash)
 (package-install 'pkg-info)
 (package-install 'cider)
-(package-install 'cider-nrepl)
 (package-install 'clojure-mode)
 
