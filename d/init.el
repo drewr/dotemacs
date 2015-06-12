fb8bef16cbdccd70c4e18eacb726ef1f0f5f2ce4
Drew Raines <drew@raines.me>
2015-06-12 08:16:12 -0500

    Don't need this anymore

--- d/init.el
+++ d/init.el
@@ -178,7 +178,6 @@
 
 ;; haskell
 
-(load "~/.emacs.d/lisp/haskell-mode/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook '(lambda ()
