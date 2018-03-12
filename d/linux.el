2fb4e4feeb86b2c869f504254888c07e4337e3f4
Drew Raines <drew@raines.me>
2018-03-12 08:40:04 -0500

    Add haskell and nix binaries

--- d/linux.el
+++ d/linux.el
@@ -1,6 +1,9 @@
 (setenv "PATH" (concat (expand-file-name "~/bin") ":"
                        (getenv "PATH")))
 
+(add-to-list 'exec-path (expand-file-name "~/.nix-profile/bin"))
+(add-to-list 'exec-path (expand-file-name "~/.local/bin"))
+
 (set-face-attribute 'default nil :family "Inconsolata" :height 120)
 
 (setq browse-url-chromium-program "google-chrome")
