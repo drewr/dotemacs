78155ecab3e245a8f99d655adb4b1b204dd9f1f4
Drew Raines <drew@raines.me>
2018-12-03 09:09:43 -0600

    After updating to Fedora 29, Emacs looks slightly smaller

--- d/linux.el
+++ d/linux.el
@@ -6,7 +6,7 @@
 (add-to-list 'exec-path (expand-file-name "~/.nix-profile/bin"))
 (add-to-list 'exec-path (expand-file-name "~/.local/bin"))
 
-(set-face-attribute 'default nil :family "Inconsolata" :height 120)
+(set-face-attribute 'default nil :family "Inconsolata" :height 130)
 
 (setq browse-url-chromium-program "google-chrome")
 (setq browse-url-browser-function 'browse-url-chromium)
