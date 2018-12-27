dbd1795cf10c81aaa28085bf88fcaf652a61fc11
Drew Raines <drew@raines.me>
2018-12-27 13:16:14 -0600

    Add unfill-paragraph

--- d/funs.el
+++ d/funs.el
@@ -228,3 +228,14 @@ and returns an org-formatted link:
   (interactive)
   (insert
    (concat "** " (upcase (format-time-string "%Y-%m-%d %a")) "\n*** ")))
+
+;; https://stackoverflow.com/a/2478549/3227
+(defun unfill-paragraph ()
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil)))
+
+(defun unfill-region ()
+  (interactive)
+  (let ((fill-column (point-max)))
+    (fill-region (region-beginning) (region-end) nil)))
