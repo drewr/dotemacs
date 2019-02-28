0bb7204fc4328158b3f6d7eb7301cd1a60308e61
Drew Raines <drew@raines.me>
2019-02-28 10:10:51 -0600

    magit-gh-pulls broke, disable for now

--- d/init.el
+++ d/init.el
@@ -161,7 +161,7 @@
 
 (use-package lua-mode       :ensure t :pin "melpa")
 (use-package magit          :ensure t :pin "melpa")
-(use-package magit-gh-pulls :ensure t :pin "melpa")
+;;(use-package magit-gh-pulls :ensure t :pin "melpa")
 (use-package markdown-mode  :ensure t :pin "melpa")
 (use-package mustache-mode  :ensure t :pin "melpa")
 (use-package nim-mode       :ensure t :pin "melpa")
