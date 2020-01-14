96cf00122b23c5eedcf44f785115fc206bd5a85b
Drew Raines <drew@raines.me>
2020-01-14 13:01:52 -0600

    Inconsolata broke. Fedora 30 and Nix Unstable are using different versions.

--- d/linux.el
+++ d/linux.el
@@ -6,7 +6,19 @@
 (add-to-list 'exec-path (expand-file-name "~/.nix-profile/bin"))
 (add-to-list 'exec-path (expand-file-name "~/.local/bin"))
 
-(set-face-attribute 'default nil :family "Inconsolata" :height 130)
+;;(set-face-attribute
+;; 'default nil
+;; :family "monospace"
+;; :height 140
+;; :width 'normal
+;; :weight 'normal)
+
+(set-face-attribute
+ 'default nil
+ :family "DejaVu Sans Mono"
+ :height 130
+ :width 'normal
+ :weight 'normal)
 
 (setq browse-url-chromium-program "google-chrome")
 (setq browse-url-browser-function 'browse-url-chromium)
