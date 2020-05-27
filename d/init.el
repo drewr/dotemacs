df4592b0776578dbb323bb4d82596a4c29178fdd
Drew Raines <drew@raines.me>
2020-05-27 17:23:04 -0500

    Add org-roam

--- d/init.el
+++ d/init.el
@@ -178,6 +178,21 @@
   :pin "melpa"
   :init (setq org-journal-dir "~/src/org/journal/"))
 
+(use-package org-roam
+  :ensure t
+  :hook
+  (after-init . org-roam-mode)
+  :custom
+  (org-roam-directory "~/.org")
+  :bind (:map org-roam-mode-map
+              (("C-c n l" . org-roam)
+               ("C-c n f" . org-roam-find-file)
+               ("C-c n j" . org-roam-jump-to-index)
+               ("C-c n b" . org-roam-switch-to-buffer)
+               ("C-c n g" . org-roam-graph))
+              :map org-mode-map
+              (("C-c n i" . org-roam-insert))))
+
 (use-package paredit
   :ensure t
   :pin "melpa"
