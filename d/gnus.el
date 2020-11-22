abfe805cba3b53444c4e4611bb3d7932303fb3cc
Drew Raines <drew@raines.me>
2020-11-22 14:59:43 -0600

    Let's not use the face anymore

--- d/gnus.el
+++ d/gnus.el
@@ -220,7 +220,7 @@ address if only it was present."
       '((".*"
          ("X-PGP-Key" "http://draines.com/pubkey.asc.txt")
          ;; Need ImageMagick!
-         (Face (gnus-face-from-file "~/.face-48.png"))
+         ;;(Face (gnus-face-from-file "~/.face-48.png"))
          (address "aaraines@gmail.com")
          ("X-MSMTP" "gmail"))
         ("elastic:.*"
