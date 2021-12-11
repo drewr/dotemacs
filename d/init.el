9472a62b7eb4a32f6ae49cf16722fa41c2924e55
Drew Raines <drew@raines.me>
2021-12-10 20:43:30 -0600

    org apparently only comes from ELPA now

--- d/init.el
+++ d/init.el
@@ -37,8 +37,6 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives
              '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 (require 'use-package)
