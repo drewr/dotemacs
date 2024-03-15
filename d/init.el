c193e4ee968165094c4025c80908fa26fe6da9e7
Drew Raines <drew@raines.me>
2024-03-15 15:04:16 -0500

    Switch generic notes to denote
    I can move them with org-attach

--- d/init.el
+++ d/init.el
@@ -563,9 +563,13 @@
          (function aar/org-capture-journal)
          :empty-lines-before 1
          :jump-to-captured t)
-        ("n" "Note" entry
-         (file+headline "notes.org" "Notes")
-         "* %u %?" :prepend t)
+        ("n" "New note" plain
+         (file denote-last-path)
+         #'denote-org-capture
+         :no-save t
+         :immediate-finish nil
+         :kill-buffer t
+         :jump-to-captured t)
         ;; For use with https://github.com/sprig/org-capture-extension
         ("p" "Protocol" entry
          (file+datetree "journal.org")
