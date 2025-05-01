d0b43b450a715ea50d3b15bf373e0b755ab1a85a
Drew Raines <drew@raines.me>
2025-05-01 09:00:54 -0500

    SASL seems to work now natively

--- d/init.el
+++ d/init.el
@@ -616,32 +616,6 @@
 (require 'erc-sasl)
 (add-to-list 'erc-sasl-server-regexp-list "irc\\.libera\\.chat")
 
-;; redefine after loading erc.el
-(defun erc-login ()
-  "Perform user authentication at the IRC server. (PATCHED, from
-https://emacs.stackexchange.com/a/50160)"
-  (erc-log (format "login: nick: %s, user: %s %s %s :%s"
-                   (erc-current-nick)
-                   (user-login-name)
-                   (or erc-system-name (system-name))
-                   erc-session-server
-                   erc-session-user-full-name))
-  (if erc-session-password
-      (erc-server-send (format "PASS %s" erc-session-password))
-    (message "Logging in without password"))
-  (when (and (featurep 'erc-sasl) (erc-sasl-use-sasl-p))
-    (erc-server-send "CAP REQ :sasl"))
-  (erc-server-send (format "NICK %s" (erc-current-nick)))
-  (erc-server-send
-   (format "USER %s %s %s :%s"
-           ;; hacked - S.B.
-           (if erc-anonymous-login erc-email-userid (user-login-name))
-           "0" "*"
-           erc-session-user-full-name))
-  (erc-update-mode-line))
-
-
-
 (require 'erc-highlight-nicknames)
 (add-to-list 'erc-modules 'log)
 (add-to-list 'erc-modules 'highlight-nicknames)
